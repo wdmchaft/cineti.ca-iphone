3352e66c985f8e71adfc2e0c846718238e78cb35
Peter McCurdy <pmccurdy@navarra.ca>
2010-10-19 18:05:14 -0400

    Add a central singleton class to store movie data.

---
+++ src/models/CinetiMovieManager.h
@@ -0,0 +1,29 @@
+//
+//  CinetiMovieManager.h
+//  Cineti
+//
+//  Created by Peter McCurdy on 10-10-18.
+//  Copyright 2010 The Navarra Group. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CinetiMovie.h"
+
+@interface CinetiMovieManager : NSObject {
+    NSMutableDictionary *dictionary;
+}
+
+- (CinetiMovie *)movieForId:(NSString *)movieid;
+- (void)addMovie:(CinetiMovie *)movie withId:(NSString *)movieid;
+
+// Methods to make this a singleton class
++ (CinetiMovieManager *)sharedInstance;
++ (id)allocWithZone:(NSZone*)zone;
+
+- (id)copyWithZone:(NSZone*)zone;
+- (id)retain;
+- (NSUInteger)retainCount;
+- (void)release;
+- (id)autorelease;
+
+@end
---
+++ src/models/CinetiMovieManager.m
@@ -0,0 +1,74 @@
+//
+//  CinetiMovieManager.m
+//  Cineti
+//
+//  Created by Peter McCurdy on 10-10-18.
+//  Copyright 2010 The Navarra Group. All rights reserved.
+//
+
+#import "CinetiMovieManager.h"
+
+@implementation CinetiMovieManager
+
+static CinetiMovieManager *sharedMovieManager = nil;
+
+- (CinetiMovie *)movieForId:(NSString *)movieid
+{
+    return (CinetiMovie *)[dictionary objectForKey:movieid];
+}
+
+- (void)addMovie:(CinetiMovie *)movie withId:(NSString *)movieid
+{
+    [dictionary setObject:movie forKey:movieid];
+}
+
+// Singleton-related methods
+
++ (CinetiMovieManager *)sharedInstance
+{
+    if (sharedMovieManager == nil) {
+        sharedMovieManager = [[super allocWithZone:NULL] init];
+    }
+    return sharedMovieManager;
+}
+
++ (id)allocWithZone:(NSZone*)zone
+{
+    return [[self sharedInstance] retain];
+}
+
+- (id)init
+{
+    if (sharedMovieManager != nil)
+        [NSException raise:@"NSMovieManager initialized twice" format:@"NSMovieManager initialized twice"];
+    // Initialization code
+    dictionary = [[NSMutableDictionary alloc] initWithCapacity:64];
+    return self;
+}
+
+- (id)copyWithZone:(NSZone*)zone
+{
+    return self;
+}
+
+- (id)retain
+{
+    return self;
+}
+
+- (NSUInteger)retainCount
+{
+    return NSUIntegerMax;
+}
+
+- (void)release
+{
+    // Do nothing
+}
+
+- (id)autorelease
+{
+    return self;
+}
+
+@end
